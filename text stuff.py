6bc045dab2ca56e7f797a8151a5549fba6ab9d1f
icebee03 <58120263+icebee03@users.noreply.github.com>
2019-11-24 00:13:37 +0100

    changed variable name

--- text stuff.py
+++ text stuff.py
@@ -7,16 +7,16 @@ def add(a):
         output.append(a[i])
         output2.insert(0,a[i])
         fr= f"{''.join(output2)} | {''.join(output)}"
-        gay= len(output)-len(a)
-        print(" "*-gay + fr)
+        hi= len(output)-len(a)
+        print(" "*-hi + fr)
 
 def remove(a):
     for i in range(len(a)):
         output.pop(-1)
         output2.pop(0)
         fr = f"{''.join(output2)} | {''.join(output)}"
-        gay = len(output) - len(a)
-        print(" " * -gay + fr)
+        hi = len(output) - len(a)
+        print(" " * -hi + fr)
 
 add(prompt)
 remove(prompt)
